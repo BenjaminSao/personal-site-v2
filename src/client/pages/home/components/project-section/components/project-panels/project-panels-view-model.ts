32aaa7b48883fa771a9ef0e0e35150348eb07a4c
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2021-09-28 13:06:44 -0400

    added animations

--- src/client/pages/home/components/project-section/components/project-panels/project-panels-view-model.ts
+++ src/client/pages/home/components/project-section/components/project-panels/project-panels-view-model.ts
@@ -1,10 +1,14 @@
-import { bind, ComponentViewModel, element, template } from "@nivinjoseph/n-app";
+import { bind, ComponentViewModel, element, events, template } from "@nivinjoseph/n-app";
 import "./project-panels-view.scss";
 
 @template(require("./project-panels-view.html"))
 @element("project-panels")
 @bind("projectsData")    
+@events("tagSelected")
 export class ProjectPanelsViewModel extends ComponentViewModel
 {
-    
+    public tagSelected(selectedTag: string)
+    {
+        this.emit("tagSelected", selectedTag);
+    }
 }
--- src/client/pages/home/components/project-section/project-section-view-model.ts
+++ src/client/pages/home/components/project-section/project-section-view-model.ts
@@ -1,13 +1,15 @@
 import { components, ComponentViewModel, element, template } from "@nivinjoseph/n-app";
+import { given } from "@nivinjoseph/n-defensive";
 import { ProjectPanelsViewModel } from "./components/project-panels/project-panels-view-model";
 import "./project-section-view.scss";
 
 @template(require("./project-section-view.html"))
 @element("project-section")
 @components(ProjectPanelsViewModel)
+
 export class ProjectSectionViewModel extends ComponentViewModel
 {
-    private readonly _ProjectsData: ReadonlyArray<ProjectData> = [
+    private readonly _projectsData: ReadonlyArray<ProjectData> = [
         {
             name: "Ecoblock",
             subtitle: "HooHacks 2021",
@@ -49,28 +51,69 @@ export class ProjectSectionViewModel extends ComponentViewModel
             description: "A walking, running, cycling route-finding app.",
             image: require("./images/journeyguide.png"),
             tags: ["JavaScript", "Vue.js", "Node.js", "Figma", "Firebase", "Google Maps API"]
+        },
+        {
+            name: "Grimsby Thai Website",
+            subtitle: "Grimsby Thai",
+            description: "Designed and created a modern looking website using Weebly for the Restuarant, Grimsby Thai Cuisine. Displays elements such as a Home and Menu page.",
+            image: require("./images/84cdcc11c8b00a7910666034a3ab68b9-grimsby-thai.png"),
+            tags: ["Weebly", "HTML", "CSS"]
+        },
+        {
+            name: "Thailandia Website",
+            subtitle: "Thailandia",
+            description: "Designed and created a modern looking website using Weebly for the Restuarant, Thailandia. Displays elements such as a Home and Menu page.",
+            image: require("./images/d7d8e861a54ab9dc205d458ca2a20748-thailandia.png"),
+            tags: ["Weebly", "HTML", "CSS"]
+        },
+        {
+            name: "Young Thai Website",
+            subtitle: "Young Thai",
+            description: "Designed and created a modern looking website using Weebly for the Restuarant, Young Thai. Displays elements such as a Home and Menu page.",
+            image: require("./images/3b54a8a0158744320dcd87ce96cff8c5-young-thai.png"),
+            tags: ["Weebly", "HTML", "CSS"]
         }
     ];
     
     private _searchKeyword: string = "";
     
+    private _uniqueTags: ReadonlyArray<string> = [];
+    
+    
     public get searchKeyword(): string { return this._searchKeyword; }
     public set searchKeyword(value: string) { this._searchKeyword = value; }
     
+    public get uniqueTags(): ReadonlyArray<string> { return this._uniqueTags; }
     
     public get projectsData(): ReadonlyArray<ReadonlyArray<ProjectData>>
     {
-        return this._convertArray(this._filterSearchResult(this._ProjectsData));
+        return this._convertArray(this._filterSearchResult(this._projectsData));
+    }
+    
+    
+    public tagSelected(selectedTag: string)
+    {
+        given(selectedTag, "selectedTag").ensureHasValue();
+        
+        this._searchKeyword = selectedTag;
+    }
+    
+    
+    protected onCreate()
+    {
+        this._uniqueTags = this._fetchUniqueTags(this._projectsData);
     }
     
     
     private _convertArray(arr: ReadonlyArray<ProjectData>): ReadonlyArray<ReadonlyArray<ProjectData>>
     {
+        const chunkSize = 2;
+        
         const result: Array<Array<ProjectData>> = [];
         
-        for (let i = 0; i < arr.length; i += 2)
+        for (let i = 0; i < arr.length; i += chunkSize)
         {
-            const chunk = arr.slice(i, i + 2);
+            const chunk = arr.slice(i, i + chunkSize);
             result.push(chunk);
         }
         
@@ -102,6 +145,22 @@ export class ProjectSectionViewModel extends ComponentViewModel
         }
         
         return false;
+    }
+    
+    private _fetchUniqueTags(arr: ReadonlyArray<ProjectData>): Array<string>
+    {
+        const uniqueArray: Array<string> = [];
+        
+        arr.forEach((projectData) =>
+        {
+            projectData.tags.forEach(tag =>
+            {
+                if (!uniqueArray.contains(tag))
+                    uniqueArray.push(tag);
+            });
+        });
+        
+        return uniqueArray;
     }
 }
 
